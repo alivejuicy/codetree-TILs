8a1527cd4dc145c88db5067d8fd3719fe996ef48
Kwak donghyun <80264449+alivejuicy@users.noreply.github.com>
2023-11-17 17:21:52 +0900

    Title: Date to Date, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

--- 231117/Date to Date/date-to-date.java
+++ 231117/Date to Date/date-to-date.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] arr = new int[]{0,31, 28, 31,30,31, 30, 31, 31, 30, 31, 30, 31};
+    static int[] arr = new int[]{0, 31, 28, 31,30,31, 30, 31, 31, 30, 31, 30, 31};
     public static void main(String[] args) throws IOException{
         // 여기에 코드를 작성해주세요.
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
@@ -15,13 +15,14 @@ public class Main {
         int d2 = Integer.parseInt(st.nextToken());
 
         
-        for(int i = 0; i<=m1; i++){
+        for(int i = 1; i<m1; i++){
             d1 += arr[i];
         }
-        for(int i = 0; i<=m2; i++){
+        for(int i = 1; i<m2; i++){
             d2 += arr[i];
         }
-        if(d1==d2) System.out.println(1);
-        else System.out.println(d2-d1);
+        
+        
+        System.out.println(d2-d1+1);
     }
 }
