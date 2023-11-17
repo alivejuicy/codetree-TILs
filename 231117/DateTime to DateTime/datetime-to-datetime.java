8ed8318dab42170dc176a49040e47cf9dae29e90
Kwak donghyun <80264449+alivejuicy@users.noreply.github.com>
2023-11-17 17:50:43 +0900

    Title: DateTime to DateTime, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231117/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int tmp = (11*24*60)+(11*60)+11;
+        int abc = (a*24*60)+(b*60)+c;
+       
+        if(abc - tmp < 0) System.out.println(-1);
+        else System.out.println(abc-tmp);
+    }
+}
