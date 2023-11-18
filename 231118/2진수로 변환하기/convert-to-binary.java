0a2418c9b8c44083a9576022a029bc5f58f9c0a9
Kwak donghyun <80264449+alivejuicy@users.noreply.github.com>
2023-11-18 21:50:45 +0900

    Title: 2진수로 변환하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231118/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(calc(n));
+    }
+
+    private static int calc(int n){
+        int[] arr = new int[100000];
+        int cnt = 0;
+
+        while(true){
+            if(n < 2){
+                arr[cnt++] = n;
+                break;
+            }
+            arr[cnt++] = n % 2;
+            n /=2;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i =cnt-1; i>=0; i--){
+            sb.append(arr[i]);
+        }
+        int tmp = Integer.parseInt(sb.toString());
+        return tmp;
+    }
+    
+}
