d7bf593b75d092a8310a18ed5b1425abd623425c
Donghyun Kwak <80264449+alivejuicy@users.noreply.github.com>
2024-08-14 15:38:05 +0900

    Title: 행복한 수열의 개수, Time: 125ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240814/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    // 연속하여 m개 이상의 동일한 원소가 나오는 순간이 존재하는 수열
+
+    static int N,M;
+    static int[][] arr;
+    public static void main(String[] args) throws IOException{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+
+       N = Integer.parseInt(st.nextToken());
+       M = Integer.parseInt(st.nextToken());
+       arr = new int[N][N];
+       // M개 이상 순서있게 동일한지 여부를 알고싶다.
+       // O(2 * n^2)
+
+       for(int i = 0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j =0; j<N; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+       }
+        int ans = 0;
+        //행
+        for(int i =0; i<N; i++){
+            ans += getRow(i);
+        }
+        //열
+        for(int i =0; i<N; i++){
+            ans+= getCol(i);
+        }
+
+        System.out.println(ans);
+    }
+    private static int getRow(int start){
+        int happy = 0;
+        for(int i = 1; i<N; i++){
+            if(arr[start][i-1] == arr[start][i] ) happy++;
+        }
+        if(happy >= M-1){
+            return 1;
+        }
+
+        return 0;
+    }
+    private static int getCol(int start){
+        int happy = 0;
+        for(int i = 1; i<N; i++){
+            if(arr[i-1][start] == arr[i][start] ) happy++;
+        }
+        if(happy >= M-1){
+            return 1;
+        }
+
+        return 0;
+    }
+}
